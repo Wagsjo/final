b77ca55db1fe6d7699f6fd7020603a3a18722a38
Johannes Wågsjö <jwagsjo@hotmail.com>
2022-06-07 09:55:25 +0200

    a

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(distPath));
 app.use('/img', express.static(path.join(__dirname, "./backend/hamsterImages/")));
-console.log("dist:" + path.join(__dirname, "./backend/hamsterImages/"));
+console.log("dist:" + distPath);
 
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`, req.body);
